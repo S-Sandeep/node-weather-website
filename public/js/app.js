b012f9f210a44a7b681078dc8390dc1325da5ba8
sandeep <sandeep.s@atcs.com>
2020-11-06 18:08:24 +0530

    remove unnecessary call logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('entering client side javascript side')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
